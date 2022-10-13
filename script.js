bbf9bce1fc2fc76baf6a545bcc6441f6e8eb784b
Ryan Papsin <ryanpapsin175@gmail.com>
2022-10-13 16:38:11 -0700

    lab8

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-
-let nameAnswer = prompt("What is your name?");
-
-let animalAnswer = prompt("Hello " + nameAnswer + ", do you like dogs and cats? yes/no");
 let adoptAnswer;
+function getname() {
 
-if (animalAnswer == "yes") {
-    adoptAnswer = prompt("Are you ready to adopt today? yes/no");
+    let nameAnswer = prompt("What is your name?");
+    return nameAnswer
 }
 
-else if (animalAnswer == "no") {
-    document.write("<b>We're sorry to hear that you don't like dogs and cats. You can still check out our awesome available animals.</b>");
+function animal() {
+
+    while (animalAnswer == "no") {
+        animalAnswer = prompt("Ummm, something went wrong! Do you like dogs and cats? yes/noyes");
+
+    }
+    if (animalAnswer == "yes") {
+        adoptAnswer = prompt("Are you ready to adopt today? yes/no");
+    }
 }
 
+function smallcatdog() {
+    let pets = prompt("how many pets is the perfect number? 1-100");
+
+    for (let i = 0; i < pets; i++) {
+
+        document.write("<img class='smallcatdog' src='smallcatdog.png' alt='Cat Dog' />");
+    }
+}
+let nameAnswer = getname();
+let animalAnswer = prompt("Hello " + nameAnswer + ", do you like dogs and cats? yes/no");
+animal()
+while (adoptAnswer == "no") {
+    adoptAnswer = prompt("Something went wrong! Are you ready to adopt TODAY! yes/no");
+}
 if (adoptAnswer == "yes") {
     document.write("<b>Outstanding! This is the perfect place for you. Take a look at our available animals for adoption</b>");
 }
-
-else if (adoptAnswer == "no") {
-    document.write("<b>Well you never know. Check out our availble animals for adoption!</b>");
-}
-
-
-
-
-
-
-
-// Function
-
-//document.write()
-//prompt()
-//console.log()
-//alert()
-
-//function addTwoNumbers(firstNumber, secondnumber){
-//    let sum = firstNumber + secondnumber;
-//}
-//
-//addTwoNumbers()
